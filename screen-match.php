0bf9b49a1779c68f5028a813f29c64e1e84bca6c
Lucas Silva <lucascarlosjobs@gmail.com>
2025-11-20 21:38:00 -0300

    string0numerica

--- screen-match.php
+++ screen-match.php
@@ -3,7 +3,8 @@ echo "Bem-vindo(a) ao screen match!";
 
 $nomeFilme = "O poderoso chefão";
 $anoLancamento = 1972;
-$notaFilme = 9.2;
+$somaDeNotas = 9 + 8 + 10 + 7 + 9 + 8.1;
+$notaFilme = $somaDeNotas / 5;
 $incluidoNoPlano = true;
 
 echo $nomeFilme;
