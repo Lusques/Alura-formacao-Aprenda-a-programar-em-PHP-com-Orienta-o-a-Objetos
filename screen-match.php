23f369bc3fbf1ef7643267ebca3b4c30c0a3defe
Lucas Silva <lucascarlosjobs@gmail.com>
2025-11-20 21:41:07 -0300

    adiciona primeiro dado adicionado pelo prompt

--- screen-match.php
+++ screen-match.php
@@ -1,10 +1,10 @@
 <?php
-echo "Bem-vindo(a) ao screen match!";
+echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "O poderoso chefão";
-$anoLancamento = 1972;
+$anoLancamento = $argv[1] ?? 1972;
 $somaDeNotas = 9 + 8 + 10 + 7 + 9 + 8.1;
 $notaFilme = $somaDeNotas / 5;
 $incluidoNoPlano = true;
 
-echo $nomeFilme;
+echo $anoLancamento;
